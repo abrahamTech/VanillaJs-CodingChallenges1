72618f2c23de674b78bad46fb95ec1ac870d3b02
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-29 21:47:49 -0600

    feat(Test1Challenge04): add a more advanced asynchronous solution, using Promise.allSettled

--- Test1-MiduliveFS/Challenge04/index.js
+++ Test1-MiduliveFS/Challenge04/index.js
@@ -53,6 +53,7 @@ export async function readFiles2() {
 
 
 // * 3.- Make them an Asynchronous function in Parallel
+//If one fails, all fail (Thats why the catch, to see the error)
 // BETTER
 
 export async function readFiles3() {
@@ -64,11 +65,47 @@ export async function readFiles3() {
         fsp.readFile('./Challenge04/files/file1.txt', 'utf8'),
         fsp.readFile('./Challenge04/files/file2.txt', 'utf8'),
         fsp.readFile('./Challenge04/files/file3.txt', 'utf8'),
-    ])
+    ]).catch(error => {
+        console.log(error);
+        return []
+    })
     
     console.timeEnd("readFiles3");
 
     return `${file1} ${file2} ${file3}`
 }
-readFiles3();
+// readFiles3();
+
+
+// * 4.- Make them an Asynchronous function in Parallel and using Promise.allSettled
+// Promise.allSettled -> It allows us to have more modular control of whether one fails or not.
+// If one fails, the others continue and we can obtain their value
+
+export async function readFiles4() {
+
+    console.time("readFiles4");
+
+    //The position of the array when we destructure it matters
+    const [file1, file2, file3] = await Promise.allSettled([
+        fsp.readFile('./Challenge04/files/file1.txt', 'utf8'),
+        fsp.readFile('./Challenge04/files/file2adas.txt', 'utf8'),
+        fsp.readFile('./Challenge04/files/file3.txt', 'utf8'),
+    ])
+    
+    console.timeEnd("readFiles4");
+   
+    //If one failed, the value will be "undefined"
+    console.log(`${file1.value} ${file2.value} ${file3.value}`);
+
+    //If one fails, its value won't be seen
+    const message = [file1.value, file2.value, file3.value]
+        .filter((value) => typeof value !== "undefined")
+        .join(" - ")
+
+    console.log(message);
+
+
+   return `${file1.value} ${file2.value} ${file3.value}`
+}
+readFiles4();
 
